ad8ef934478cd20c22b82983b088cb6da037d718
Bhagyalakshmi-patil <spbhagya2k77@gmail.com>
2025-05-12 14:33:21 +0530

    11th May Task

---
+++ src/Task1_Printf/Printf_exercise.java
@@ -0,0 +1,18 @@
+package Task1_Printf;
+
+public class Printf_exercise {
+    public static void main(String[] args) {
+        int a = 5;
+       String new_line = "\n";
+        System.out.printf("%d*1 = 5",a);
+        System.out.printf("%s%d*2 = 10",new_line,a);
+        System.out.printf("%s%d*3 = 15",new_line,a);
+        System.out.printf("%s%d*4 = 20",new_line,a);
+        System.out.printf("%s%d*5 = 25",new_line,a);
+        System.out.printf("%s%d*6 = 30",new_line,a);
+        System.out.printf("%s%d*7 = 35",new_line,a);
+        System.out.printf("%s%d*8 = 40",new_line,a);
+        System.out.printf("%s%d*9 = 45",new_line,a);
+        System.out.printf("%s%d*10 = 50",new_line,a);
+    }
+}
